a88fed0419beb6d147a23f1f50f845522f4a1f05
Justin Clark <jclark@cyber.law.harvard.edu>
2011-07-27 17:53:21 -0400

    make page header same as header everywhere else

--- header-page.php
+++ header-page.php
@@ -74,7 +74,7 @@
 <div id="page" class="hfeed">
 	<header id="branding" role="banner">
 			<hgroup>
-				<h1 id="site-title"><span><a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></span></h1>
+				<h1 id="site-title"><span><a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><img src="<?php echo get_stylesheet_directory_uri() . '/images/youthandmedia-logo.png'; ?>" alt="<?php bloginfo( 'name' ); ?>" /></a></span></h1>
 				<h2 id="site-description"><?php bloginfo( 'description' ); ?></h2>
 			</hgroup>
 
