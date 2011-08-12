0d69f353b2def283b671d54840f55b7fe2a6f364
Justin Clark <jclark@cyber.law.harvard.edu>
2011-08-12 11:26:12 -0400

    added mouse scrubbing

--- js/page.js
+++ js/page.js
@@ -1,4 +1,13 @@
 jQuery(document).ready(function() {
+    var scrubber = {
+        $list: jQuery('#mycarousel'),
+        $container: jQuery('#viz1'),
+        bufferDist: 50,
+        velocityConst: 1.8,
+        deadZoneWidth: 150,
+        changeCursor: true
+    };
+
     jQuery('.youtube-video-link').click(function(e) {
         e.preventDefault();
         var target = jQuery(this).attr('href'),
@@ -14,18 +23,41 @@ jQuery(document).ready(function() {
             overlayClose:true
         });
     });
-    jQuery('#viz1').mousemove(function(e) {
-        var containerWidth = jQuery('#viz1').width(),
-            listWidth = jQuery('#mycarousel').width();
-        if (listWidth > containerWidth){
-            var mousePos = 1,
-                diff = e.pageX - mousePos;
-            if (diff > 10 || diff < -10) { 
-                mousePos = e.pageX; 
-                newX = (containerWidth - listWidth) * (e.pageX/containerWidth);
-                diff = parseInt(Math.abs(parseInt(jQuery('#mycarousel').css('left'))-newX )).toFixed(0);
-                jQuery('#mycarousel').stop().animate({'left':newX}, {duration:diff*3});
+
+    jQuery('#viz1').bind({
+        mousemove: function(e) {
+            if (scrubber.$list.width() > scrubber.$container.width() && Math.abs(scrubber.$list.data('startX') - e.pageX) > scrubber.bufferDist) {
+                var pxFromCenter = e.pageX - (parseInt(scrubber.$container.offset().left) + parseInt(scrubber.$container.width() / 2)),
+                    minLeft = - (scrubber.$list.width() - scrubber.$container.width()),
+                    maxLeft = 0,
+                    velocity = pxFromCenter * scrubber.velocityConst,
+                    distance = (velocity < 0) ? parseInt(scrubber.$list.css('left')) : -minLeft + parseInt(scrubber.$list.css('left')),
+                    time = (distance / velocity).toFixed(2);
+                //console.log(distance + '/' + time + '=' + velocity);
+                scrubber.$list.data('startX', e.pageX);
+                if (Math.abs(pxFromCenter) <= parseInt(scrubber.deadZoneWidth / 2)) {
+                    scrubber.$list.stop(true);
+                    if (scrubber.changeCursor) {
+                        scrubber.$container.css({ cursor: 'auto' });
+                    }
+                }
+                else {
+                    if (scrubber.changeCursor) {
+                        scrubber.$container.css({
+                            cursor: (pxFromCenter > 0) ? 'e-resize' : 'w-resize'
+                        });
+                    }
+                    //Probably should do this with a step function instead
+                    scrubber.$list.stop(true).animate(
+                        { left: (velocity < 0) ? maxLeft : minLeft },
+                        { duration: time * 1000, easing: 'linear' }
+                    );
+                }
             }
+        },
+
+        mouseenter: function(e) {
+            scrubber.$list.data('startX', e.pageX);
         }
     });
 });
