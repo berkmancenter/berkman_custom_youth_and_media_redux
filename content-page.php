7016ea19cf79975209e342102364f12add115e8c
Justin Clark <jclark@cyber.law.harvard.edu>
2011-10-06 17:06:01 -0400

    replaced breadcrumb plugin with a few lines

--- content-page.php
+++ content-page.php
@@ -6,18 +6,32 @@
  * @subpackage Twenty_Eleven
  * @since Twenty Eleven 1.0
  */
+
+function get_breadcrumb($id)
+{
+    $post = get_post($id);
+    $breadcrumb_posts = array(esc_attr(get_the_title($id)));
+
+    while ($post->post_parent >= 0 && $post->post_parent != false && $id != $post->post_parent && get_option('page_on_front') != $post->post_parent) {
+        $id = $post->post_parent;
+        $post = get_post($id);
+        $breadcrumb_posts[] = '<a href="' . esc_url(get_permalink($id)) . '" title="' . esc_attr(get_the_title($id)) . '">' . esc_attr(get_the_title($id)) . '</a>';
+    }
+
+    if (count($breadcrumb_posts) > 1) {
+        return implode(esc_html(' > '), array_reverse($breadcrumb_posts));
+    }
+    else {
+        return '';
+    }
+}
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header">
 		<h1 class="entry-title"><?php the_title(); ?></h1>
 		<div class="breadcrumbs">
-		<?php
-		if(function_exists('bcn_display'))
-		{
-			bcn_display();
-		}
-		?>
+		<?php echo get_breadcrumb(get_the_ID()); ?>
 		</div>
 	</header><!-- .entry-header -->
 
