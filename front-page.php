f24568aacbbbe48ce592e52c7b144acd041e62e7
Justin Clark <jclark@cyber.law.harvard.edu>
2011-10-06 15:23:45 -0400

    changed language again

--- front-page.php
+++ front-page.php
@@ -33,7 +33,7 @@ get_header(); ?>
 
 		<div id="primary">
                 <?php if ( ! empty( $filters ) ): ?>
-				<h2>Explore some of Youth and Media's highlights. Sort by:</h2>
+				<h2>Explore some of Youth and Media's highlights. Only show:</h2>
 				<div id="filters">
                     <?php foreach ($filters as $filter): ?>
                     <label for="<?php echo strtolower($filter); ?>">
